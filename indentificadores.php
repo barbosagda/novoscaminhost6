35939ec88e29b0aaae9d35d6c02708c54c81d91f
Rodrigo Cezario <rodrigocezario@msn.com>
2022-06-28 21:07:03 -0300

    Aula do encontro dia 28/0/2022

---
+++ indentificadores.php
@@ -0,0 +1,61 @@
+<?php
+
+//string (texto literal)
+$nome = "Rodrigo";
+$nome = 43;
+
+$TAXA = 10;
+
+define('TAXA_IMPOSTO', 1.5);
+
+echo TAXA_IMPOSTO;
+
+
+
+$nome_da_disciplina = "Linguagem PHP";
+
+$nomeDaDisciplina = "Linguagem PHP";
+
+//sensitive case
+$Nome = "Rodrigo2";
+
+$_nome = "";
+
+$nome = "";
+
+$texto = "Aqui é um texto $nome";
+
+//integer (inteiro)
+$idade = 43;
+
+
+//float (decimal)
+$altura = 1.87;
+
+//nulo
+$nada = null;
+
+
+$nome = "";
+
+$dataNascimentoDia = 28;
+$dataNascimentoMes = 6;
+$dataNascimentoAno = 2022;
+
+//Boolean (valor lógico) sim/não
+$hojeTemAula = false; //false
+
+//array (vetor indexado)
+$lista = ['primeiro', 'segundo', 'terceiro'];
+
+//object
+$objeto = new stdClass;
+
+//resource
+$conexaoBancoDados = mysql_connection('', '', '');
+
+//echo 'Esse é um texto simples com apostofo '. $nome;
+
+
+
+
---
+++ olamundo.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Rodrigo";
+
+echo "Olá mundo com PHP.<br>";
+
+echo "Segunda linha";
+
+echo "Meu nome é $nome<br>";
+
+echo 'Essa é uma saída de tela com apostofo! $nome\n';
